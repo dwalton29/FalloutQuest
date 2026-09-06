3bfd03c30910fe835c797cb2b67ca63c1a333412
dwalton29 <131550347+dwalton29@users.noreply.github.com>
2026-09-06 08:22:26 +0100

    Add OpenXR Android platform prelude header

---
+++ app/src/main/cpp/openxr-platform-prelude.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// OpenXR's Android/OpenGL ES platform declarations require these platform
+// types to be visible before openxr_platform.h is parsed.
+#include <jni.h>
+#include <EGL/egl.h>
